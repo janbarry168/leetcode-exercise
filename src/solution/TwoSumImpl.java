fbdd0f96d4fb9468f3118ec28513d774d7b5e260
Barry Jan <janbarry5168@gmail.com>
2022-04-29 17:48:27 +0800

    #1 Two Sum - rename function

--- src/solution/TwoSumImpl.java
+++ src/solution/TwoSumImpl.java
@@ -9,7 +9,7 @@ public class TwoSumImpl implements TwoSum {
 
     @Override
     public int[] twoSum(int[] nums, int target) {
-        return solution2(nums, target);
+        return solution2_twoSum(nums, target);
     }
 
     /**
@@ -17,7 +17,7 @@ public class TwoSumImpl implements TwoSum {
      * time: O(n^2)
      * space: O(1)
      */
-    private int[] solution1(int[] nums, int target) {
+    private int[] solution1_twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -33,7 +33,7 @@ public class TwoSumImpl implements TwoSum {
      * time: O(n)
      * space: O(n)
      */
-    private int[] solution2(int[] nums, int target) {
+    private int[] solution2_twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int currIndex = 0; currIndex < nums.length; currIndex++) {
             int matchValue = target - nums[currIndex];
